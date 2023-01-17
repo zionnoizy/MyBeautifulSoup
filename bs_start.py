2ed7237baefa6068172a6b17960965a5687f7cb4
zionnoizy <zion95123@outlook.com>
2023-01-17 11:18:41 +0000

    firstname lastname flip

--- bs_start.py
+++ bs_start.py
@@ -7,17 +7,39 @@ import lxml.html as PARSER
 with open("fullname.html") as fp:
     soup = BeautifulSoup(fp, "lxml")
 
-# print (soup)
-
+# this flip lastname, firstname to firstname lastname
+ 
 soup = BeautifulSoup(open("fullname.html"), "lxml")
 
-mydivs = soup.find_all("a", text=True)
+empty_arr = []
 
+data = []
+table = soup.find('table')
+table_body = table.find('tbody')
+allprojects = table_body.find_all("a", text=True)
+# rows = table_body.find_all('tr')
 
+for i, allprojects in enumerate(allprojects):
 
-for i, mydivs in enumerate(mydivs):
-    print (str(i) + ": " + str(mydivs.string))
+    empty_arr.append(str(allprojects.string))
+
+# print (table_body)
+for fl in empty_arr:
+
+    last, first = fl.split(', ')
+
+    tmp = first + last
+    modified = tmp.replace("  ", " ")
+    print(modified)
+
+    # seperator = ' '
+    # x = seperator.join([first, last])
+    # print (x)
+
+    # 
+    # seperator.join(tmp1)
+    # print (tmp1)
 
 
 # fullname = soup.find_all("a")
-# print (fullname.string)
+# print (table_body.string)
---
+++ flip_last_first.py
@@ -0,0 +1,5 @@
+
+
+
+for last_name, first_name in [q.split("," for q. in s.split(";"))]
+    print (first_name, last_name)
